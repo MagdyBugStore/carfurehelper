21e1baefb5eb4cb0bd0188761cd0c04f99c72cd4
Ahmed magdy <ahmedmegaa1@gmail.com>
2024-06-12 03:52:57 +0300

    update err

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const axios = require('axios');
 const { MongoClient } = require('mongodb');
-const axios = require('axios');
 
 async function refshreshWebsite() {
   try {
